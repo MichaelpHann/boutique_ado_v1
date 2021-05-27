561dcf633c7db54f0aa102ac07973eec5f93ca25
MichaelpHann <michael.hann@live.co.uk>
2021-05-27 11:46:16 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -62,9 +62,9 @@ def product_detail(request, product_id):
     A view to show individual product details
     """
     product = get_object_or_404(Product, pk=product_id)
-    
+
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
